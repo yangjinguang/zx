aad020b8ea5e5de463005cd68488b101f9fd9919
杨金光 <yangjg@jiedaibao.com>
2016-09-15 18:06:27 +0800

    修改server api

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,6 +1,6 @@
 angular.module('zxApp')
     .constant('CONFIG', {
-        API:{
-            url:'http://127.0.0.1:6543',
+        API: {
+            url: 'http://123.57.45.81:6543',
         }
     });
