dc43405881b8ceb8a4a7b334e9b8688b228bc6fb
杨金光 <yangjg@jiedaibao.com>
2016-12-08 19:20:51 +0800

    登录认证

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,7 +8,7 @@
  * Controller of the zxApp
  */
 angular.module('zxApp')
-    .controller('LoginCtrl', function ($scope, $state, $http, AuthApi) {
+    .controller('LoginCtrl', function ($scope, $state, $location, $http, AuthApi) {
         var data = $scope.data = {};
         var userData = $scope.userData = {};
         var fn = $scope.fn = {
@@ -18,7 +18,7 @@ angular.module('zxApp')
                     localStorage.userName = res.name;
                     localStorage.token = res.token;
                     $http.defaults.headers.common['authorization'] = res.token;
-                    $state.go('main')
+                    $location.path('/#/')
                 }, function (err) {
                     data.err = err.data.data
                 })
